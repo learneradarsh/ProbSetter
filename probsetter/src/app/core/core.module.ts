4f3c6140201e2649a3a451d92d135014c7926b8d
Adarsh Learner <learner.adarsh@gmail.com>
2018-02-08 15:38:27 +0530

    initial commit

---
+++ probsetter/src/app/core/core.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NavbarComponent, FooterComponent]
+})
+export class CoreModule { }
